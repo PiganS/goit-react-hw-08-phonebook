714b2efe3358ab70a68220c5f62a412388acbd21
PiganS <130235012+PiganS@users.noreply.github.com>
2023-12-09 01:17:16 +0000

    Deploying to gh-pages from @ PiganS/goit-react-hw-08-phonebook@b1676686deac7dd79488ea4b5d8bbfe955a5e31e 🚀

--- static/js/415.6acea1f4.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{5415:function(e,r,t){t.r(r);var o=t(6966),n=(t(2791),t(3329));r.default=function(){return(0,n.jsx)("div",{children:(0,n.jsx)(o.vN,{children:"Hello! I'm your phonebook"})})}}}]);
-//# sourceMappingURL=415.6acea1f4.chunk.js.map
---
+++ static/js/415.9227cd1b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{5415:function(e,r,t){t.r(r);var a=t(6966),c=(t(2791),t(3329));r.default=function(){return(0,c.jsxs)("div",{children:[(0,c.jsx)(a.vN,{children:"Hello! I'm your phonebook"}),(0,c.jsx)(a.X0,{children:"keep all your contacts in a safe place"})]})}}}]);
+//# sourceMappingURL=415.9227cd1b.chunk.js.map
